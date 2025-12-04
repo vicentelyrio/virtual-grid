be9f1ec194c65fc6d5d48b91c79b0672ea969f3b
vicentelyrio <vicentelyrio@gmail.com>
2025-12-04 08:49:53 -0300

    feat(docs): creating basic example

---
+++ apps/docs/examples/basic/basic.tsx
@@ -0,0 +1,28 @@
+import { useVirtualGrid } from '@virtual-grid/lib'
+
+import classes from './basic.module.css'
+
+const data = Array(1000).fill(0).map((_, i) => i + 1)
+
+export function BasicGrid() {
+  const { items, styles, gridRef, scrollRef } = useVirtualGrid({
+    data,
+    gap: 16,
+    padding: [16, 16, 16, 16],
+  })
+
+  return (
+    <div ref={scrollRef} className={classes.container}>
+      <div
+        ref={gridRef}
+        style={styles}
+        className={classes.grid}>
+        {items?.map((item) => (
+          <div key={item} className={classes.card}>
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
